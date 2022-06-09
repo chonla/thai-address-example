67e4d897e9d3687e554445a6434d8e788b6a5ff3
Chonlasith Jucksriporn <chonlasith@gmail.com>
2022-06-10 00:14:13 +0700

    chore: Update examples

--- index.js
+++ index.js
@@ -2,4 +2,20 @@ const ThaiAddr = require('@chonla/thai-address').default;
 
 const thaiAddr = new ThaiAddr();
 
+console.log('All zip codes');
 console.log(thaiAddr.zips());
+
+console.log('All provinces');
+console.log(thaiAddr.provinces());
+
+console.log('Get entries which has zip code 10130');
+console.log(thaiAddr.findByZip('10130'));
+
+console.log('Get districts in สมุทรปราการ');
+console.log(thaiAddr.districts('11000000'));
+
+console.log('Get subdistricts in พระประแดง');
+console.log(thaiAddr.subdistricts('11040000'));
+
+console.log('Get subdistrict data of บางยอ');
+console.log(thaiAddr.findBySubdistrictCode('11040800'));
