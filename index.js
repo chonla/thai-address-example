18ab836e50a6f54cb80c9d7782e9fe724d7a605e
Chonlasith Jucksriporn <chonlasith@gmail.com>
2023-01-16 17:22:58 +0700

    fix: Backward compaitible

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
 const ThaiAddr = require('@chonla/thai-address').default;
 
+const thaiAddr = new ThaiAddr();
+
 console.log('Data version');
-console.log(ThaiAddr.dataVersion());
+console.log(thaiAddr.dataVersion());
 
 console.log('All zip codes');
-console.log(ThaiAddr.zips());
+console.log(thaiAddr.zips());
 
 console.log('All provinces');
-console.log(ThaiAddr.provinces());
+console.log(thaiAddr.provinces());
 
 console.log('Get entries which has zip code 10130');
-console.log(ThaiAddr.findByZip('10130'));
+console.log(thaiAddr.findByZip('10130'));
 
 console.log('Get districts in สมุทรปราการ');
-console.log(ThaiAddr.districts('11000000'));
+console.log(thaiAddr.districts('11000000'));
 
 console.log('Get subdistricts in พระประแดง');
-console.log(ThaiAddr.subdistricts('11040000'));
+console.log(thaiAddr.subdistricts('11040000'));
 
 console.log('Get subdistrict data of บางยอ');
-console.log(ThaiAddr.findBySubdistrictCode('11040800'));
+console.log(thaiAddr.findBySubdistrictCode('11040800'));
