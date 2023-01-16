bd6ee56c6f8f42fff27752efd18eb68a4e5df6b5
Chonlasith Jucksriporn <chonlasith@gmail.com>
2023-01-16 17:06:11 +0700

    feat: Update to 1.1.0

--- index.ts
+++ index.ts
@@ -1,21 +1,22 @@
 import ThaiAddr from "@chonla/thai-address";
 
-const thaiAddr = new ThaiAddr();
+console.log('Data version');
+console.log(ThaiAddr.dataVersion());
 
 console.log('All zip codes');
-console.log(thaiAddr.zips());
+console.log(ThaiAddr.zips());
 
 console.log('All provinces');
-console.log(thaiAddr.provinces());
+console.log(ThaiAddr.provinces());
 
 console.log('Get entries which has zip code 10130');
-console.log(thaiAddr.findByZip('10130'));
+console.log(ThaiAddr.findByZip('10130'));
 
 console.log('Get districts in สมุทรปราการ');
-console.log(thaiAddr.districts('11000000'));
+console.log(ThaiAddr.districts('11000000'));
 
 console.log('Get subdistricts in พระประแดง');
-console.log(thaiAddr.subdistricts('11040000'));
+console.log(ThaiAddr.subdistricts('11040000'));
 
 console.log('Get subdistrict data of บางยอ');
-console.log(thaiAddr.findBySubdistrictCode('11040800'));
+console.log(ThaiAddr.findBySubdistrictCode('11040800'));
